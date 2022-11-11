ec30296e97c34ab2a29c70a3d380aa664eef9f27
agiordanoge1 <70514835+agiordanoge1@users.noreply.github.com>
2022-11-11 15:53:30 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import requests as req
+import plotly.express as px
 url = "https://data.ademe.fr/data-fair/api/v1/datasets/dpe-france"
 resp =req.get(url)
 print(resp.text)
+
+fig = px.bar(x=["a", "b", "c"], y=[1, 3, 2])
+fig.show()
