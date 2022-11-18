00d3df36b274f3031277c2aadee19e968a7e3f26
agiordanoge1 <angelo.giordano@universite-paris-saclay.fr>
2022-11-18 15:14:42 +0100

    wip(histogramme)

--- histograme.py
+++ histograme.py
@@ -1,3 +1,30 @@
 import plotly.express as px
+from Dataset import Dataset
 
-figHist = px.bar(x=["a", "b", "c"], y=[1, 3, 2])
+
+t = Dataset(select=("nom_methode_dpe","classe_estimation_ges"), size=10000)
+t = t.getData()
+
+
+d= dict()
+for i in t["results"]:
+    if(i["classe_estimation_ges"] in d):
+        d[i["classe_estimation_ges"]] += 1
+    else:
+        d[i["classe_estimation_ges"]] = 1
+
+print(d)
+
+nbechantillon=0
+nbechantillon=10000-d['N']
+print(nbechantillon)
+
+
+
+
+#total=total/10000
+
+
+
+
+figHist = px.histogram(x=["A", "B", "C", "D","E","F","G"], y=[d['A'], d['B'], d['C'], d['D'], d['E'], d['F'],d['G']])
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import plotly.express as px
 import dash
 from map import figMap
 from histograme import figHist
-
+from histograme import nbechantillon
 
 app = dash.Dash(__name__)
 
@@ -12,17 +12,22 @@ app.layout = html.Div([
     html.H1('DPE in France'),
 
     html.Div([
-        html.Div([html.Div([html.H3("Carte de la France")], className="card-header"), html.P("Select a candidate:"), dcc.Graph(figure=figHist), dcc.RadioItems(
+        html.Div([html.Div([html.H3("Histograme par commune")], className="card-header"), html.P("Select an histogram: ("+nbechantillon+" échantillons)"), dcc.Graph(figure=figHist), dcc.RadioItems(
             id='candidate',
             options=["Joly", "Coderre", "Bergeron"],
             value="Coderre",
             inline=True
         ), ], className="card"),
-        html.Div([html.Div([html.H3("Histograme par commune")],
+        html.Div([html.Div([html.H3("Histograme par commune")], className="card-header"), html.P("Select an histogram: ("+nbechantillon+" échantillons)"), dcc.Graph(figure=figHist), dcc.RadioItems(
+            id='candidate',
+            options=["Joly", "Coderre", "Bergeron"],
+            value="Coderre",
+            inline=True
+        ), ], className="card"),
+        html.Div([html.Div([html.H3("Carte de la France")],
                  className="card-header"), dcc.Graph(figure=figMap)], className="card"),
     ], id="globalContainer"),
 
 ])
-
 
 app.run_server(debug=True)
