31712397d38f065876d7f1d79b196468f6dd083d
agiordanoge1 <angelo.giordano@universite-paris-saclay.fr>
2022-11-23 10:18:17 +0100

    wip(historigramme)

--- histograme.py
+++ histograme.py
@@ -2,11 +2,13 @@ import plotly.express as px
 from Dataset import Dataset
 
 
-t = Dataset(select=("nom_methode_dpe","classe_estimation_ges"), size=10000)
+t = Dataset(select=("annee_construction","classe_estimation_ges"), size=100)
 t = t.getData()
 
 
 d= dict()
+dannee=dict()
+
 for i in t["results"]:
     if(i["classe_estimation_ges"] in d):
         d[i["classe_estimation_ges"]] += 1
@@ -14,17 +16,38 @@ for i in t["results"]:
         d[i["classe_estimation_ges"]] = 1
 
 print(d)
+print(t)
 
 nbechantillon=0
 nbechantillon=10000-d['N']
 print(nbechantillon)
 
+#for i in t["results"]:
+    #if(i["annee_construction"] in dannee):
+    #   dannee[i["annee_construction"]] += 1
+    #else:
+    #    dannee[i["annee_construction"]] = 1
+preAnner = 0
+for da in t["results"]:
+    if da["annee_construction"] < preAnner:
+        continue
+    if(da["annee_construction"] in dannee):
+        for i in t["results"]:
+            preAnner = da["annee_construction"]+10
+            if(da["annee_construction"]<i["annee_construction"]<da["annee_construction"]+10):
+                    if(da["annee_construction"] in dannee):
+                        dannee[da["annee_construction"]] += 1
+                    else:
+                        dannee[da["annee_construction"]] = 1
+                
 
-
+print(dannee)
 
 #total=total/10000
 
 
 
 
-figHist = px.histogram(x=["A", "B", "C", "D","E","F","G"], y=[d['A'], d['B'], d['C'], d['D'], d['E'], d['F'],d['G']])
+#figHist = px.histogram(x=["A", "B", "C", "D","E","F","G"], y=[d['A'], d['B'], d['C'], d['D'], d['E'], d['F'],d['G']])
+
+#figHist2 = px.histogram(x=["1950","1960"], y=[dannee['195'],dannee['1961']])
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import plotly.express as px
 import dash
 from map import figMap
 from histograme import figHist
+from histograme import figHist2
 from histograme import nbechantillon
 
 app = dash.Dash(__name__)
@@ -13,6 +14,12 @@ app.layout = html.Div([
 
     html.Div([
         html.Div([html.Div([html.H3("Histograme par commune")], className="card-header"), html.P("Select an histogram: ("+str(nbechantillon)+" échantillons)"), dcc.Graph(figure=figHist), dcc.RadioItems(
+            id='candidate',
+            options=["Joly", "Coderre", "Bergeron"],
+            value="Coderre",
+            inline=True
+        ), ], className="card"),
+        html.Div([html.Div([html.H3("Histograme par commune")], className="card-header"), html.P("Select an histogram: ("+str(nbechantillon)+" échantillons)"), dcc.Graph(figure=figHist2), dcc.RadioItems(
             id='candidate',
             options=["Joly", "Coderre", "Bergeron"],
             value="Coderre",
