41d8326ffc70506660e20081dc2ce70e016cf620
domesday <m.andriamiraho@gmail.com>
2022-12-21 20:02:31 +0100

    wip(map)

--- main.py
+++ main.py
@@ -2,33 +2,23 @@ from dash import dcc, html, Input, Output
 import plotly.express as px
 import dash
 from map import figMap
-# from histograme import figHist
 from histograme import figHist2
 from histograme import nbechantillon
-
+import dash_leaflet as dl
 app = dash.Dash(__name__)
 
 
 app.layout = html.Div([
-    html.H1('DPE in France'),
-
+    html.H1('DPE en France'),
     html.Div([
-        # html.Div([html.Div([html.H3("Histograme par commune")], className="card-header"), html.P("Select an histogram: ("+str(nbechantillon)+" échantillons)"), dcc.Graph(figure=figHist), dcc.RadioItems(
-        #     id='candidate',
-        #     options=["Joly", "Coderre", "Bergeron"],
-        #     value="Coderre",
-        #     inline=True
-        # ), ], className="card"),
-        html.Div([html.Div([html.H3("Estimation GES par foyer en Kg eqCO2/m².an")], className="card-header"), html.P("Select an histogram: ( "+str(nbechantillon)+"échantillons)"), dcc.Graph(figure=figHist2), dcc.RadioItems(
+        html.Div([html.Div([html.H3("Estimation GES par foyer en Kg eqCO2/m².an"), dcc.RadioItems(
             id='candidate',
             options=["Joly", "Coderre", "Bergeron"],
             value="Coderre",
             inline=True
-        ), ], className="card"),
-        html.Div([html.Div([html.H3("Carte de la France")],
-                 className="card-header"), dcc.Graph(figure=figMap)], className="card"),
-    ], id="globalContainer"),
-
+        )], className="card-header"), dcc.Graph(figure=figHist2)], className="card hist1"),
+        html.Div([html.Div([html.H3("Carte de la France")], className="card-header"), figMap], className="card map"),
+    ], className="globalContainer"),
 ])
 
 app.run_server(debug=True)
--- map.py
+++ map.py
@@ -1,10 +1,3 @@
-import plotly.express as px
+import dash_leaflet as dl
 
-df = px.data.election()  # replace with your own data source
-geojson = px.data.election_geojson()
-figMap = px.choropleth(
-    df, geojson=geojson,
-    locations="district", featureidkey="properties.district",
-    projection="mercator", range_color=[0, 6500])
-figMap.update_geos(fitbounds="locations", visible=False)
-figMap.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
+figMap = dl.Map(dl.TileLayer(), style={'width': '100%', 'height': '500px'})
