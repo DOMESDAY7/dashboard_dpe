82004990c679f4a210edf6f9bda0e6e374ccd02d
agiordanoge1 <angelo.giordano@universite-paris-saclay.fr>
2022-12-22 17:58:17 +0100

    wip(localisation_histogramme)

--- histogramme2.py
+++ histogramme2.py
@@ -2,7 +2,7 @@ import plotly.express as px
 from Dataset import Dataset
 
 
-t = Dataset(select=("annee_construction","classe_estimation_ges","estimation_ges"), size=10000)
+t = Dataset(select=("annee_construction", "tv016_departement_code","classe_estimation_ges","estimation_ges"), size=10000)
 # print(t.dumpURL())
 t = t.getData()
 # print(sorted((k,sorted(v.items()))for k,v in t.items))
@@ -17,6 +17,14 @@ dgesbyten["1950"]=0
 dgesbyten["1970"]=0
 dgesbyten["2000"]=0
 dgesbyten["2010"]=0
+
+departlocalisation=dict()
+dlocalisation=dict()
+dlocalisation["Paris"]=0
+dlocalisation["Marseille"]=0
+dlocalisation["t"]=0
+dlocalisation["aris"]=0
+dlocalisation["ris"]=0
 
 dannee=dict()
 danneetribyten=dict()
@@ -33,18 +41,19 @@ nbechantillon2=0
 
 #recuperation du nombre de foyers par annee de construction et un total des estimations ges pour faire la moyenne ensuite 
 for i in t["results"]:
-    if(i["annee_construction"] in dannee):
-      dannee[i["annee_construction"]] += 1
-      dges[i["annee_construction"]] += i["estimation_ges"]
-      #nbechantillon2=nbechantillon2+1
+    print(i)
+    if(i["tv016_departement_code"] in departlocalisation):
+      departlocalisation[i["tv016_departement_code"]] += 1
+      #dges[i["annee_construction"]] += i["estimation_ges"]
+      nbechantillon2=nbechantillon2+1
     else:
-       dannee[i["annee_construction"]] = 1
-       dges[i["annee_construction"]] = i["estimation_ges"]
-       #nbechantillon2=nbechantillon2+1
+       departlocalisation[i["tv016_departement_code"]] = 1
+       #dges[i["annee_construction"]] = i["estimation_ges"]
+       nbechantillon2=nbechantillon2+1
       
 
-for i in sorted(dannee.keys()):
-    print(i,dannee[i],dges[i])
+for i in sorted(departlocalisation.keys()):
+    print(i,departlocalisation[i])
     nbechantillon2=nbechantillon2+1
 
 #echantillonage avec des tranches d annees pour condenser les donnees en abscisses
@@ -75,16 +84,16 @@ for i in sorted(dannee.keys()):
       nbechantillon=nbechantillon+dannee[i] 
       
 
-print(nbechantillon)      
+print(nbechantillon2)      
     # print(i,dannee[i])
 #moyenne des estimations ges total/nombre de foyers
-moyenne=dict()
-moyenne["1700"]=dgesbyten["1700"]/danneetribyten["1700"]
-moyenne["1900"]=dgesbyten["1900"]/danneetribyten["1900"]
-moyenne["1950"]=dgesbyten["1950"]/danneetribyten["1950"]
-moyenne["1970"]=dgesbyten["1970"]/danneetribyten["1970"]
-moyenne["2000"]=dgesbyten["2000"]/danneetribyten["2000"]
-moyenne["2010"]=dgesbyten["2010"]/danneetribyten["2010"]
+# moyenne=dict()
+# moyenne["1700"]=dgesbyten["1700"]/danneetribyten["1700"]
+# moyenne["1900"]=dgesbyten["1900"]/danneetribyten["1900"]
+# moyenne["1950"]=dgesbyten["1950"]/danneetribyten["1950"]
+# moyenne["1970"]=dgesbyten["1970"]/danneetribyten["1970"]
+# moyenne["2000"]=dgesbyten["2000"]/danneetribyten["2000"]
+# moyenne["2010"]=dgesbyten["2010"]/danneetribyten["2010"]
 
 
-figHist2 = px.histogram(x=["1700-1899","1900-1949","1950-1969","1970-2000","2000-2009","2010-2015"], y=[moyenne['1700'],moyenne['1900'],moyenne['1950'],moyenne["1970"],moyenne['2000'],moyenne["2010"]])
+# figHist2 = px.histogram(x=["1700-1899","1900-1949","1950-1969","1970-2000","2000-2009","2010-2015"], y=[moyenne['1700'],moyenne['1900'],moyenne['1950'],moyenne["1970"],moyenne['2000'],moyenne["2010"]])
