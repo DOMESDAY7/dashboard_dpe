398a2c92db2396d5e23410bbeebe048a4f4d4f29
DOMESDAY <79838340+DOMESDAY7@users.noreply.github.com>
2022-12-23 09:05:21 +0000

    feat(faq)/style(global)

--- main.py
+++ main.py
@@ -17,8 +17,14 @@ figMap = figMap.getMap()
 
 
 app.layout = html.Div([
-    html.H1('DPE en France'),
-    html.P('Nombre d\'échantillon : '+str(nbEchantillon)),
+    # header
+    html.Header([
+        html.H1('DPE en France'),
+        html.P('Nombre d\'échantillon : '+str(nbEchantillon)),
+    ]),
+
+
+    # main
     html.Div([
         html.Div([html.Div([html.H3("Estimation GES par foyer en Kg eqCO2/m².an"), dcc.RadioItems(
             id='candidate',
@@ -27,9 +33,24 @@ app.layout = html.Div([
             inline=True
         )], className="card-header"), dcc.Graph(figure=figHist)], className="card hist1"),
         html.Div([html.Div([html.H3("Carte de la France")],
-                 className="card-header"), 
+                 className="card-header"),
                  dcc.Graph(figure=figMap)], className="card map"),
     ], className="global-container"),
+
+    # FAQ
+    html.Section([
+        html.H3("FAQ"),
+        html.Details([
+            html.Summary("D'ou proviennent les données ?"),
+            html.P(["Les données de ce dashboard proviennent d'une base de donnée réalisé par l'observatoire du DPE en France ",
+                    html.A(
+                        "ADEME.", href="https://www.data.gouv.fr/fr/organizations/ademe/")])], className="card"),
+        html.Div(html.Details([html.Summary("Qu'est-ce que le DPE ?"),
+                               html.P("Le DPE est le diagnostique des performance énergétique, il sert principalement à évaluer la quantité d'énergie et de gaz à effet de serre d'un logement.")], className="card")),
+        html.Div(html.Details([html.Summary("C'est indice est-il important ?"),
+                               html.P("Oui, bien évidemment l'indice de diagnostique des performances énergétique")], className="card"))
+    ],className="faq")
+
 ], className="wraper")
 
 app.run_server(debug=True)
