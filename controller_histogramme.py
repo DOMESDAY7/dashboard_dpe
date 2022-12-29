92221ef2bacfa11fd125adfef7fa1f7e5c2b0079
agiordanoge1 <angelo.giordano@universite-paris-saclay.fr>
2022-12-29 15:36:19 +0100

    update(un_appel_histo)

--- controller_histogramme.py
+++ controller_histogramme.py
@@ -1,10 +1,16 @@
 from Dataset import Dataset
 from model_histogramme import Histogramme
+from model_histogramme2 import Histogramme2
+
+data = Dataset(select=("annee_construction", "latitude", "longitude",
+               "classe_consommation_energie", "estimation_ges", "tv016_departement_code", "geo_adresse"), size=10000)
 
 def update(year=2020):
-    data = Dataset(select=("annee_construction", "latitude", "longitude",
-               "classe_estimation_ges", "estimation_ges", "tv016_departement_code", "geo_adresse"), size=10000)
 
+    
     histo = Histogramme(data, year=year)
     histo = histo.get_histo()
-    return histo
+
+    histo2=Histogramme2(data)
+    histo2=histo2.get_histo2()
+    return histo, histo2
